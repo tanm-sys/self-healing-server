c3a7b939889dc9b0e545d71ea4e7f41093e974a6
tanm-sys <66236112+tanm-sys@users.noreply.github.com>
2024-08-14 18:21:18 +0530

    Update prometheus_metrics.py

--- prometheus_metrics.py
+++ prometheus_metrics.py
@@ -1,15 +1,9 @@
 from prometheus_client import start_http_server, Gauge
 
-class PrometheusMetrics:
-    def __init__(self):
-        self.cpu_usage_gauge = Gauge('cpu_usage', 'CPU Usage')
-        self.memory_usage_gauge = Gauge('memory_usage', 'Memory Usage')
-        self.disk_usage_gauge = Gauge('disk_usage', 'Disk Usage')
-        self.response_time_gauge = Gauge('response_time', 'Response Time')
-        start_http_server(8000)
+cpu_usage_gauge = Gauge('cpu_usage', 'CPU usage percentage')
+memory_usage_gauge = Gauge('memory_usage', 'Memory usage percentage')
+disk_usage_gauge = Gauge('disk_usage', 'Disk usage percentage')
+response_time_gauge = Gauge('response_time', 'Response time in seconds')
 
-    def update_metrics(self, health_data):
-        self.cpu_usage_gauge.set(health_data['cpu_usage'])
-        self.memory_usage_gauge.set(health_data['memory_usage'])
-        self.disk_usage_gauge.set(health_data['disk_usage'])
-        self.response_time_gauge.set(health_data['response_time'])
+def setup_prometheus_metrics():
+    start_http_server(8001)
