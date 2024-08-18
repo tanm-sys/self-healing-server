ca7b03c7f2e5fbda011ad8e93d0c3bc5b13852e0
Tanmay patil <66236112+tanm-sys@users.noreply.github.com>
2024-08-18 15:39:22 +0530

    Update app.py

--- web_dashboard/app.py
+++ web_dashboard/app.py
@@ -1,23 +1,10 @@
-from flask import Flask, render_template, request
-import sqlite3
-import structlog
-
-logger = structlog.get_logger()
+from flask import Flask, render_template
 
 app = Flask(__name__)
-DATABASE = 'web_dashboard/health_data.db'
-
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    data = conn.execute('SELECT * FROM health_data').fetchall()
-    conn.close()
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
